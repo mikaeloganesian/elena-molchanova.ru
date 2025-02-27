3893463d1c1a9d190a2575081dcbd3dddbdc8502
mikaeloganesan <maoganesian@edu.hse.ru>
2025-02-27 19:45:40 +0300

    realisation commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Menu from './components/Menu';
+import Hero from './components/Hero';
+import Quotation from './components/Quotation';
+import AboutMe from './components/AboutMe';
+import Skills from './components/Skills';
+import Companies from './components/Companies';
+import Achivments from './components/Achivments';
+import ContactMe from './components/ContactMe';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <Hero />
+      <Quotation />
+      <AboutMe />
+      <Skills />
+      <Companies />
+      <Achivments />
+      <ContactMe />
+      <Footer />
+    </div>
+  );
+}
+
+export default App; 
